44304c232243986dba38d290004bd5ddcac4af14
pfranczak <gzem89@gmail.com>
2017-10-18 21:21:14 +0200

    Create Domain

---
+++ j2billsDomain/src/main/java/pl/j2dev/pojo/JournalMovement.java
@@ -0,0 +1,20 @@
+package pl.j2dev.pojo;
+
+public class JournalMovement {
+
+	Journal source;
+	Journal target;
+	public Journal getSource() {
+		return source;
+	}
+	public void setSource(Journal source) {
+		this.source = source;
+	}
+	public Journal getTarget() {
+		return target;
+	}
+	public void setTarget(Journal target) {
+		this.target = target;
+	}
+	
+}
